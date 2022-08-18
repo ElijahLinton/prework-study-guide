45773a960360cec70e8b5caf419b7bca49f901b7
ElijahLinton <demigodlinty@gmail.com>
2022-08-17 20:44:17 -0500

    add functions and for loops within our branch

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,40 @@
-var topics = ['HTML', 'CSS' , 'Git', 'JavaScript'];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(var x = 0; x < topics.length; x++){
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-    console.log(topics[x]);
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
 
+function selectTopic() {
+ if (randomTopic === 'HTML') {
 
-/*
+   console.log("Let's study HTML!");
 
-if (topic === 'HTML') {
+  } else if (randomTopic === 'CSS') {
 
-    console.log("Let's study HTML!");
+   console.log("Let's study CSS!");
 
-} else if (topic === 'CSS') {
+  } else if (randomTopic === 'Git') {
 
-    console.log("Let's study CSS!");
+   console.log("Let's study Git!");
 
-} else if (topic === 'Git') {
+  } else if (randomTopic === 'JavaScript') {
 
-    console.log("Let's study Git!");
+   console.log("Let's study JavaScript!");
 
-} else if (topic === 'Javascript') {
+  } else {
 
-    console.log('please try again!');
-
+   console.log('Please try again!');
+  }
 }
 
-*/
+console.log('Here are the topics we learned through prework')
+
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopic();
